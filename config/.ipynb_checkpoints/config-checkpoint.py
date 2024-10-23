a52fa31f259cf5b029de761554f743c679762690
Aakash <aakash.panda@inmobi.com>
2024-10-23 17:45:03 +0530

    remove unwanted file

--- config/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,8 +0,0 @@
-CONFIG = {
-    'feature_top7' : ['Churn Value', 'Churn Score', 'Tenure Months',
-                'Internet Service_Fiber optic', 'Contract_Two year',
-                'Payment Method_Electronic check', 'Dependents_Yes'],
-    'data_path' : 'data.csv',
-    'save_model_path': 'model_filename.pkl',
-    
-}
